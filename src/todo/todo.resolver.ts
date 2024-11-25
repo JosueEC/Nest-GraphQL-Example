1a5d5e9c436667c150f9b364c03dd3b2f9dbf953
JosueEC <josueev.cruz@gmail.com>
2024-11-25 17:52:35 -0600

    [FEAT](todo.resolver): Define the find-all resolver method

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,10 +1,19 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Todo } from './entity/todo.entity';
 
 @Resolver()
 export class TodoResolver {
-  @Query(() => [String])
-  findAll() {
-    return [];
+  @Query(() => [Todo], {
+    name: 'todos',
+  })
+  findAll(): Todo[] {
+    return [
+      {
+        todoId: 1,
+        description: 'uno',
+        done: false,
+      },
+    ];
   }
 
   findOne() {
