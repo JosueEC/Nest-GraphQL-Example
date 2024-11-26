b64477e961e3ddb28d45110bb5a0e660e5e0ebcc
JosueEC <josueev.cruz@gmail.com>
2024-11-25 18:18:54 -0600

    [FEAT](todo.resolver): Inject the todos-service and add the find-one resolver method

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,23 +1,45 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { TodoService } from './todo.service';
 import { Todo } from './entity/todo.entity';
 
 @Resolver()
 export class TodoResolver {
+  constructor(private readonly todosService: TodoService) {}
+
   @Query(() => [Todo], {
     name: 'todos',
   })
   findAll(): Todo[] {
-    return [
-      {
-        todoId: 1,
-        description: 'uno',
-        done: false,
-      },
-    ];
+    return this.todosService.findAll();
   }
 
-  findOne() {
-    return [];
+  /**
+   * Uso de 'fragment' en queries de Graphql para evitar la repetición
+   * de argumentos en consultas:
+   * 
+   * {
+   *   todo1: todo(todoId: 1) {
+   *     ...todoFields
+   *   }
+   *   todo2: todo(todoId: 2) {
+   *     ...todoFields
+   *   }
+   *   todo3: todo(todoId: 3) {
+   *     ...todoFields
+   *   }
+   * }
+
+   * fragment todoFields on Todo {
+   *   todoId
+   *   done
+   *   description
+   * }
+   */
+  @Query(() => Todo, {
+    name: 'todo',
+  })
+  findOne(@Args('todoId', { type: () => Int }) todoId: number): Todo {
+    return this.todosService.findOne(todoId);
   }
 
   create() {
